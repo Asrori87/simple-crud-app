474bac31be4ad4144f9b89d86c50d288591c832b
Asrori <fauzanasrori87@gmail.com>
2024-11-05 18:52:16 +0000

    simple crud app with express js

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const productRoute = require("./routes/product.route.js");
+
+const app = express();
+const PORT = 3000;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello fron Node API Server Updated!");
+});
+
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`)
+);
+
+// Connection to mongodb
+mongoose
+  .connect(
+    "mongodb+srv://nanami:nanami123@backenddb.8ejr5.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => console.log("Connected to database!"))
+  .catch(() => console.log("Connected failed!"));
